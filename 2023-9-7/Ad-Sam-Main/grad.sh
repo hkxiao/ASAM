504495e7a2a0ddf885970ff34f8244c74283cbf6
xhk <hk.xiao.me@gmail.com>
2023-11-01 14:41:54 -0400

    xx

--- 2023-9-7/Ad-Sam-Main/grad.sh
+++ 2023-9-7/Ad-Sam-Main/grad.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#CUDA_VISIBLE_DEVICES_LIST=(0 1 2 3 4 5 6 7)
-CUDA_VISIBLE_DEVICES_LIST=(0)
-now=1
-interval=11188
+#CUDA_VISIBLE_DEVICES_LIST=(0 1 2 3 4 5)
+# CUDA_VISIBLE_DEVICES_LIST=(1)
+CUDA_VISIBLE_DEVICES_LIST=(6 7)
+now=10000
+interval=600
 
 for id in "${CUDA_VISIBLE_DEVICES_LIST[@]}"
 do
@@ -17,19 +18,21 @@ do
     --control_mask_dir=/data/tanglv/data/sam-1b/sa_000000/four_mask \
     --caption_path=/data/tanglv/data/sam-1b/sa_000000-blip2-caption.json \
     --controlnet_path=ckpt/control_v11p_sd15_mask_sa000000@4.pth \
+    --inversion_dir=output/sa_000000@4-Inversion/embeddings \
     --sam_batch=4 \
     --model_type=vit_b \
     --guidance_scale=9.0 \
     --ddim_steps=20 \
     --eps=0.2 \
-    --alpha=0.02 \
     --steps=10 \
+    --alpha=0.01 \
     --mu=0.5 \
-    --gamma=10 \
-    --beta=0.1 \
+    --beta=1.0 \
+    --norm=2 \
+    --gamma=100 \
     --start=${now} \
     --end=$((now + interval)) &
-    now=$(expr $now + $interval)
+    now=$(expr $now + $interval) 
 done
 
 wait
