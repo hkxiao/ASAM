8f612d39c1e8d8eaba376b07f9f868b0b35e4610
xhk <hk.xiao.me@gmail.com>
2023-11-01 18:40:16 +0000

    controlnet

--- 2023-9-7/Ad-Sam-Main/ControlNet-main/tutorial_train.py
+++ 2023-9-7/Ad-Sam-Main/ControlNet-main/tutorial_train.py
@@ -12,6 +12,8 @@ import tensorboard
 parser  = argparse.ArgumentParser()
 parser.add_argument('--gpus',type=int)
 parser.add_argument('--dataset',type=str)
+parser.add_argument('--batchsize',type=int)
+parser.add_argument('--epoch',type=int)
 parser.add_argument('--json_path',type=str)
 parser.add_argument('--resume',type=str, default='models/control_sd15_ini.ckpt')
 args = parser.parse_args()
@@ -19,7 +21,7 @@ args = parser.parse_args()
 
 # Configs
 resume_path = args.resume
-batch_size = 4
+batch_size = args.batchsize
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
@@ -37,9 +39,8 @@ model.only_mid_control = only_mid_control
 dataset = MyDataset(root=args.dataset,json_path=args.json_path)
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-# trainer = pl.Trainer(strategt, gpus=args.gpus, precision=32, callbacks=[logger])
 
-trainer = pl.Trainer(strategy="ddp", accelerator="gpu", devices=args.gpus, precision=32, callbacks=[logger]) 
+trainer = pl.Trainer(strategy="ddp", accelerator="gpu", devices=args.gpus, precision=32, callbacks=[logger], max_epochs=args.epoch) 
 
 
 # Train!
