cb8a999562a6e67b63d2b5785c51a20c4c759e5e
xhk <hk.xiao.me@gmail.com>
2023-11-02 02:58:27 +0000

    x

--- 2023-9-7/Ad-Sam-Main/grad.sh
+++ 2023-9-7/Ad-Sam-Main/grad.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#CUDA_VISIBLE_DEVICES_LIST=(0 1 2 3 4 5 6)
-#CUDA_VISIBLE_DEVICES_LIST=(1)
-CUDA_VISIBLE_DEVICES_LIST=(6 7)
-now=1
-interval=6000
+#CUDA_VISIBLE_DEVICES_LIST=(0 1 2 3 4 5)
+# CUDA_VISIBLE_DEVICES_LIST=(1)
+CUDA_VISIBLE_DEVICES_LIST=(6)
+now=7485
+interval=600
 
 for id in "${CUDA_VISIBLE_DEVICES_LIST[@]}"
 do
@@ -18,20 +18,16 @@ do
     --control_mask_dir=/data/tanglv/data/sam-1b/sa_000000/four_mask \
     --caption_path=/data/tanglv/data/sam-1b/sa_000000-blip2-caption.json \
     --controlnet_path=ckpt/control_v11p_sd15_mask_sa000000@4.pth \
-    --inversion_dir=/data/tanglv/xhk/Ad-Sam/2023-9-7/Ad-Sam-Main/output/sa_000000@4-Inversion/embeddings \
     --sam_batch=4 \
-    --model_type=vit_b \
-    --guidance_scale=9.0 \
-    --ddim_steps=20 \
     --eps=0.2 \
     --steps=10 \
-    --alpha=0.02 \
+    --alpha=0.01 \
     --mu=0.5 \
-    --beta=0.1 \
+    --beta=1.0 \
     --norm=2 \
-    --gamma=10 \
+    --gamma=100 \
     --start=${now} \
-    --end=$((now + interval)) &
+    --end=$((now + interval)) 
     now=$(expr $now + $interval) 
 done
 
