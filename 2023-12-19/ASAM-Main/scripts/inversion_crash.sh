abb4ae53fcd0ac4332fe5cbfd2399c1ecf5922d1
xhk <hk.xiao.me@gmail.com>
2024-01-05 23:29:48 +0800

    grad

--- 2023-12-19/ASAM-Main/scripts/inversion_crash.sh
+++ 2023-12-19/ASAM-Main/scripts/inversion_crash.sh
@@ -6,7 +6,7 @@ start_program() {
     --data_root=sam-1b/sa_000000 \
     --caption_path=sam-1b/sa_000000-blip2-caption.json \
     --guidance_scale=7.5 \
-    --steps=10 \
+    --steps=5 \
     --ddim_steps=50 \
     --start=$3 \
     --end=$4  
@@ -14,8 +14,8 @@ start_program() {
 
 # 设置GPU列表
 CUDA_VISIBLE_DEVICES_LIST=(0 1 2 3 4 5 6 7)
-start=(1 500 1000 1500 2000 2500 3002 3500)
-end=(500 1000 1500 2000 2500 3000 3500 4000)
+start=(4000 4500 5000 5500 6000 6500 7000 7500)
+end=(5400 5000 5500 6000 6500 7000 7500 8000)
 
 PID_LIST=()
 STATUS=()
