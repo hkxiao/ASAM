5aa100c92caad86238e8b3e75e2449acf1c56fa7
xhk <hk.xiao.me@gmail.com>
2024-01-05 23:38:11 +0800

    xxx

--- 2023-9-7/Ad-Sam-Main/sam_continue_learning/scripts/test.sh
+++ 2023-9-7/Ad-Sam-Main/sam_continue_learning/scripts/test.sh
@@ -1,8 +1,5 @@
-# export NCCL_DEBUG=INFO
-# export NCCL_DEBUG_SUBSYS=ALL
-# export TORCH_DISTRIBUTED_DEBUG=INFO
-export CUDA_VISIBLE_DEVICES=3,4,5
-python -m torch.distributed.launch --nproc_per_node=3 --master_port=30001  tuning.py \
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=30001  tuning.py \
 --model-type vit_b \
 --output_prefix work_dirs/diceloss_sam_iou_masktoken-tuning_b_adv@4 \
 --batch_size_train=8 \
@@ -12,11 +9,11 @@ python -m torch.distributed.launch --nproc_per_node=3 --master_port=30001  tunin
 --numworkers=0 \
 --restore-model work_dirs/diceloss_sam_iou_masktoken-tuning_b_adv@4/epoch_9.pth \
 --eval \
---prompt_type point \
+--prompt_type box \
 --train-datasets dataset_sa000000 \
---valid-datasets dataset_camo dataset_big_val dataset_BBC038v1 dataset_DOORS1 dataset_DOORS2 dataset_ZeroWaste dataset_ndis_train dataset_Plittersdorf_test dataset_egohos dataset_LVIS \
-# --visualize \
-#--baseline
+--valid-datasets dataset_hrsod_val dataset_camo dataset_big_val dataset_BBC038v1 dataset_DOORS1 dataset_DOORS2 dataset_ZeroWaste dataset_ndis_train dataset_Plittersdorf_test dataset_egohos dataset_LVIS \
+--visualize \
+--baseline
 #--valid-datasets dataset_camo dataset_big_val dataset_BBC038v1 dataset_DOORS1 dataset_DOORS2 dataset_ZeroWaste dataset_ndis_train dataset_Plittersdorf_test dataset_egohos dataset_LVIS \
 #--valid-datasets dataset_hrsod_val dataset_ade20k_val dataset_voc2012_val dataset_cityscapes_val dataset_coco2017_val dataset_camo dataset_big_val dataset_BBC038v1 dataset_DOORS1 dataset_DOORS2 dataset_ZeroWaste dataset_ndis_train dataset_Plittersdorf_test dataset_egohos dataset_LVIS \
 #--valid-datasets dataset_ZeroWaste dataset_ndis_train dataset_Plittersdorf_test dataset_egohos dataset_LVIS \
