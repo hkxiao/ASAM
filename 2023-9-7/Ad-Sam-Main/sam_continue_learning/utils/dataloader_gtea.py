88cc87b955a89fae1dd3011b20acd81bc36158de
xhk <hk.xiao.me@gmail.cm>
2024-03-24 20:06:50 +0800

    xxx

--- 2023-9-7/Ad-Sam-Main/sam_continue_learning/utils/dataloader_gtea.py
+++ 2023-9-7/Ad-Sam-Main/sam_continue_learning/utils/dataloader_gtea.py
@@ -71,7 +71,7 @@ class GTEADataset(Dataset):
         
         unique_list = sorted(np.unique(naive_gt))        
         gt = np.empty((0,naive_gt.shape[0],naive_gt.shape[1]))
-        
+        #print(unique_list)
         for i in unique_list:
             if i==0: continue
             tmp_gt = np.zeros((naive_gt.shape[0],naive_gt.shape[1]))     
