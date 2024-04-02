e2ae8da780aefdfca6696771846b59a92e3e94a0
xhk <hk.xiao.me@gmail.com>
2024-04-02 15:50:52 +0800

    clear

--- 2023-9-7/Ad-Sam-Main/inversion.sh
+++ 2023-9-7/Ad-Sam-Main/inversion.sh
@@ -2,8 +2,8 @@
 
 CUDA_VISIBLE_DEVICES_LIST=(0 1 2 3 4 5 6 7)
 #CUDA_VISIBLE_DEVICES_LIST=(0)
-now=11187
-interval=250
+now=1
+interval=1250
 
 for id in "${CUDA_VISIBLE_DEVICES_LIST[@]}"
 do
@@ -13,10 +13,10 @@ do
     export CUDA_VISIBLE_DEVICES=${id} 
     python null_text_inversion.py \
     --save_root=output/sa_000001-Inversion \
-    --data_root=sam-1b/sa_000001 \
-    --control_mask_dir=sam-1b/sa_000001 \
-    --caption_path=sam-1b/sa_000001-blip2-caption.json \
-    --controlnet_path=ckpt/control_v11p_sd15_mask_sa000002.pth \
+    --data_root=../../2023-12-19/ASAM-Main/sam-1b/sa_000001 \
+    --control_mask_dir=../../2023-12-19/ASAM-Main/sam-1b/sa_000001 \
+    --caption_path=../../2023-12-19/ASAM-Main/sam-1b/sa_000001-blip2-caption.json \
+    --controlnet_path=../../2023-12-19/ASAM-Main/ckpt/control_v11p_sd15_mask_sa000001.pth \
     --guidence_scale=7.5 \
     --steps=10 \
     --ddim_steps=50 \
